23389f06f9a03084b95867d71477e275a5794b19
shohelm909 <64063886+shohelm909@users.noreply.github.com>
2024-03-31 15:23:29 +0600

    districtiing part 01

--- script.js
+++ script.js
@@ -1,75 +1,10 @@
-/*var x = 10;
-var y = 5;
-var z = x + y;
-var myObj = {
-    name: "Javascript",
-    founder: "Brendan Eich",
-    estd: "1995",
-    randkin: 1,
-    x: x,
-    y: y,
-    z: z,
-}*/
-var x = 10;
-var y = 5;
-var z = x + y;
-var myObj = {
-    name: "Javascript",
-    founder: "Brendan Eich",
-    estd: "1995",
-    randkin: 1,
-    x,
-    y,
-    z,
+var user ={
+    id:339,
+    name: "Sakib",
+    age: 35,
 }
+//var name = user["name"];
+//const {name}=user;
+const {name:title} = user;
 
-document.write(myObj.name);
-var keys = Object.keys(myObj);
-document.write(keys);
-console.log(keys);
-var values = Object.values(myObj);
-console.log(values);
-var entries = Object.entries(myObj);
-console.log(entries);
-var properties = {
-    name: "Javascript",
-    date: "1995",
-    founder: "Brenden"
-}
-
-for (x in properties){
-    console.log(properties[x]);
-}
-
-let element = [1,2,3,4,5,6,7,8,];
-
-
-/*for(x in element){
-    console.log(x)
-}*/
-for (x of element){
-    console.log(x);
-    console.log(element[x])
-}
-
-let myStr = "I love to play with javascript";
-
-for ( x in  myStr){
-    console.log(x);
-}
-for(x of myStr){
-    console.log(x);
-}
-var obj = {
-    name: "Javascript",
-    date: "1995",
-    founder: "Brenden"
-}
-
-var keys = Object.keys(obj);
-console.log(keys)
-
-var values = Object.values(obj);
-console.log(values);
-var entries = Object.entries(obj);
-console.log(entries);
+console.log(title);
