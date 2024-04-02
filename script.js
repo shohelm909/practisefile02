f0979637b87f393a2d1d549622bbde51800c4999
shohelm909 <64063886+shohelm909@users.noreply.github.com>
2024-04-02 12:47:41 +0600

    tagged template literals

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
+//We have Sakib Tamim in our cricket team(add Mr. before every name)
 function modifier(string, ...values){
-    console.log(string);
-    console.log(values)
+    const m = string.reduce((accum,current)=>{
+        return accum + current + (values.length ? "Mr. " + values.shift(): "");
+    }, "")
+    return m;
 }
+
+
 
 var player1 = "Sakib";
 var player2 = "Tamim";
 
-console.log(modifier`We have ${player1} and ${player2} in our cricket team`)
 
-var num = [1,2,3];
-var num2 = num.unshift(4);
-
-var num1 = num.map((x)=>{
-    return x*x;
-})
-
-console.log(num1);
-
-
-var sum = num.reduce((accum,current)=>{
-    return accum * current;
-},5)
-
-console.log(sum);
+console.log(modifier`We have ${player1} and ${player2} in our cricket team.`);
