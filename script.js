9fe04ce7ea3aa48592e5273a3ac1dd7d59219e51
shohelm909 <64063886+shohelm909@users.noreply.github.com>
2024-04-02 12:23:00 +0600

    reduce function indetail

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-//import {pi as sh, a as bh} from "./external.js";
-//console.log(sh,bh);
-import external, {pi,} from "./external.js";
-console.log(external)
-console.log(pi)
+function modifier(string, ...values){
+    console.log(string);
+    console.log(values)
+}
 
-external()
+var player1 = "Sakib";
+var player2 = "Tamim";
+
+console.log(modifier`We have ${player1} and ${player2} in our cricket team`)
+
+var num = [1,2,3];
+var num2 = num.unshift(4);
+
+var num1 = num.map((x)=>{
+    return x*x;
+})
+
+console.log(num1);
+
+
+var sum = num.reduce((accum,current)=>{
+    return accum * current;
+},5)
+
+console.log(sum);
